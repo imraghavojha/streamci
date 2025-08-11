93eb1e3292a13fd72ebf2ce4fad26cfc8763b964
RaghavOjha <jeh267@txstate.edu>
2025-08-10 20:32:27 -0500

    Initial commit

---
+++ src/main/java/com/yourname/streamci/streamci/StreamciApplication.java
@@ -0,0 +1,13 @@
+package com.yourname.streamci.streamci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourname/streamci/streamci/StreamciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourname.streamci.streamci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
